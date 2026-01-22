ec5c0542f13db61e14069f19cce1d4d03b59cc78
David Salvador Castellanos <david.salvador.castellanos.dsc@gmail.com>
2026-01-22 19:43:36 +0100

    Repositorio (Interfaz y Clase)

---
+++ src/main/java/agenda/repositorios/ContactoRepository.java
@@ -0,0 +1,13 @@
+package agenda.repositorios;
+
+import agenda.entidades.Contacto;
+
+import java.util.List;
+
+public interface ContactoRepository {
+    List<Contacto> obtenerTodos();
+    Contacto obtenerPorId(Long id);
+    Contacto guardar(Contacto contacto);
+    void eliminar(Long id);
+}
+
